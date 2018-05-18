b5acfffcd54224478a1d2c79333e071f7c7781d5
YangLiu <yang.liu92@qq.com>
2018-05-18 09:00:00 +0800

    添加groovy作为动态规则引擎

---
+++ intsmaze-groovy/src/main/java/org/intsmaze/groovy/RuleConfigModel.java
@@ -0,0 +1,84 @@
+package org.intsmaze.groovy;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class RuleConfigModel implements Serializable {
+
+	private static final long serialVersionUID = -9038328210279400521L;
+
+	private Long id;
+	private String ruleName;
+	private String ruleDesc;
+	private String ruleScript;
+	private String hashcode;
+	private Integer ruleType;  // 规则类型
+
+	public RuleConfigModel(Long id, String ruleName, String ruleDesc,
+			String ruleScript, String hashcode, Integer ruleType) {
+		super();
+		this.id = id;
+		this.ruleName = ruleName;
+		this.ruleDesc = ruleDesc;
+		this.ruleScript = ruleScript;
+		this.hashcode = hashcode;
+		this.ruleType = ruleType;
+	}
+
+	public Integer getRuleType() {
+		return ruleType;
+	}
+
+	public void setRuleType(Integer ruleType) {
+		this.ruleType = ruleType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRuleName() {
+		return ruleName;
+	}
+
+	public void setRuleName(String ruleName) {
+		this.ruleName = ruleName;
+	}
+
+	public String getRuleDesc() {
+		return ruleDesc;
+	}
+
+	public void setRuleDesc(String ruleDesc) {
+		this.ruleDesc = ruleDesc;
+	}
+
+
+	public String getRuleScript() {
+		return ruleScript;
+	}
+
+	public void setRuleScript(String ruleScript) {
+		this.ruleScript = ruleScript;
+	}
+
+
+	public String getHashcode() {
+		return hashcode;
+	}
+
+	public void setHashcode(String hashcode) {
+		this.hashcode = hashcode;
+	}
+
+	public RuleConfigModel() {
+		super();
+	}
+
+	  
+
+}
---
+++ intsmaze-groovy/src/main/java/org/intsmaze/groovy/test/ExecuteGroovy.java
@@ -0,0 +1,51 @@
+package org.intsmaze.groovy.test;
+
+import groovy.lang.Binding;
+
+import org.intsmaze.groovy.CacheGroovyService;
+import org.intsmaze.groovy.GScriptExecutor;
+import org.intsmaze.groovy.GroovyDemo;
+import org.intsmaze.groovy.RuleCalc;
+import org.intsmaze.groovy.StormBeanFactory;
+import org.junit.Test;
+
+/**
+ * @author:YangLiu
+ * @date:2018年5月9日 上午10:03:06
+ * @describe:
+ */
+public class ExecuteGroovy {
+
+	@Test
+	public void test() {
+		StormBeanFactory beanFactory = new StormBeanFactory("redis-base.xml");
+		CacheGroovyService cacheService = beanFactory.getBean(CacheGroovyService.class);
+		cacheService.start();
+		GScriptExecutor scriptExecutor = beanFactory.getBean(GScriptExecutor.class);
+		
+		Binding shellContext = new Binding();
+		shellContext.setVariable("AGE", "100");
+		shellContext.setVariable("GROOVY", new GroovyDemo());
+		
+		//*********************配置脚本核心计算处*******************
+		RuleCalc calc = new RuleCalc();
+		calc.authRuleCalc(scriptExecutor, shellContext); // 核心计算
+		//*********************配置脚本核心计算处*******************
+		while(true)
+		{
+			
+		}
+		
+		
+//		Binding binding = new Binding();
+//		GroovyShell shell = new GroovyShell(binding);
+//		Script scrpt = shell.parse("script.groovy");
+//
+//		binding.setVariable("str1", "value1");
+//		binding.setVariable("str2", "value2");
+//		binding.setVariable("newConcat", scrpt);
+//		System.out
+//				.println(scrpt.evaluate("newConcat.customConcat(str1, str2)"));
+//		System.out.println(scrpt.evaluate("str1.concat(str2)"));
+	}
+}
